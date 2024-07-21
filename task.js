e81c64060ad3b56e4459aa61501d9ae26e4dc5e0
Ankur Pandey <pankur216@gmail.com>
2024-07-21 03:59:39 -0400

    making no changes but checking if lazy.git works

--- task.js
+++ task.js
@@ -6,33 +6,33 @@ const path = require('path');
 const port = 3000;
 
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }))
 
 app.use(express.urlencoded({
-    extended: true
+  extended: true
 }))
 
 let global_db = {}; //Creating a mini database to store all the tasks
 let taskNum;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/data', (req, res) => {
-    res.json(global_db);
+  res.json(global_db);
 })
 
 app.post('/write', (req, res) => {
-    taskNum = Object.keys(global_db).length + 1;
-    const formData = req.body;
-    global_db[taskNum] = formData;
-    console.log("The database now after adding task " + taskNum + ":-");
-    console.log(global_db);
-    res.redirect('/');
+  taskNum = Object.keys(global_db).length + 1;
+  const formData = req.body;
+  global_db[taskNum] = formData;
+  console.log("The database now after adding task " + taskNum + ":-");
+  console.log(global_db);
+  res.redirect('/');
 })
 
 app.listen(port, () => {
-    console.log(`Started listening on port: ${port}`);
+  console.log(`Started listening on port: ${port}`);
 })
